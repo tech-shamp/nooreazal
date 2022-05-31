3e852d837529e5c6ad036e6746331eca6f738a9c
tech-shamp <shampytechnical@gmail.com>
2022-06-01 01:33:44 +0500

    Elem Del

--- js/app.js
+++ js/app.js
@@ -1,13 +1,2 @@
 const log = console.log;
 log("NooreAzal Foundation");
-
-// Map API Configuration
-let map;
-function initMap() {
-  lahoreMap = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-
-window.initMap = initMap;
