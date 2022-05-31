21e962a4ef564e4b309ad84117bbe8d790acf79e
tech-shamp <shampytechnical@gmail.com>
2022-06-01 01:02:22 +0500

    minor

--- js/app.js
+++ js/app.js
@@ -1,22 +1,13 @@
 const log = console.log;
 log("NooreAzal Foundation");
 
-// // Map API Configuration
-// let map;
-// function initMap() {
-//   lahoreMap = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 8,
-//   });
-// }
+// Map API Configuration
+let map;
+function initMap() {
+  lahoreMap = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
 
-// window.initMap = initMap;
-
-// Swiper Library
-const swiper = new Swiper(".mySwiper", {
-  pagination: {
-    clickable: true,
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-  },
-});
+window.initMap = initMap;
