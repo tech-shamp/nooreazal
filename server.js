13648f25c75b6712e5eb70f593170b8cbcfd47bc
tech-shamp <shampytechnical@gmail.com>
2022-06-03 21:31:45 +0500

    server

--- server.js
+++ server.js
@@ -9,14 +9,17 @@ app.use(express.static(initialPath));
 app.get("/", (req, res) => {
   res.sendFile(path.join(initialPath, "index.html"));
 });
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(initialPath, "about.html"));
-});
 app.get("/blog", (req, res) => {
   res.sendFile(path.join(initialPath, "blog.html"));
 });
 app.get("/contact", (req, res) => {
   res.sendFile(path.join(initialPath, "contact.html"));
+});
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(initialPath, "about.html"));
+});
+app.get("/editor", (req, res) => {
+  res.sendFile(path.join(initialPath, "editor.html"));
 });
 app.get("/donate", (req, res) => {
   res.sendFile(path.join(initialPath, "donate.html"));
